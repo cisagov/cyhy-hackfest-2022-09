851f8cf9ca5b987a4a64c577b9c18c6cdd900583
David Redmin <david.redmin@gwe.cisa.dhs.gov>
2023-08-30 09:59:11 -0400

    Temporarily comment out required inputs and outputs
    This should allow the GitHub actions related to Terraform initialization and configuration validation to complete successfully.

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,4 @@
-output "api" {
-  value       = aws_appsync_graphql_api.hackfest_chat
-  description = "The AppSync API."
-}
+# output "api" {
+#   value       = aws_appsync_graphql_api.hackfest_chat
+#   description = "The AppSync API."
+# }
--- variables.tf
+++ variables.tf
@@ -4,25 +4,25 @@
 # You must provide a value for each of these parameters.
 # ------------------------------------------------------------------------------
 
-variable "appsync_service_role_arn" {
-  description = "The ARN of the IAM role that has permissions to access the data sources."
-  type        = string
-}
+# variable "appsync_service_role_arn" {
+#   description = "The ARN of the IAM role that has permissions to access the data sources."
+#   type        = string
+# }
 
-variable "dynamodb_table_names" {
-  description = "The names of the DynamoDB tables supporting the API."
-  type        = map(string)
-}
+# variable "dynamodb_table_names" {
+#   description = "The names of the DynamoDB tables supporting the API."
+#   type        = map(string)
+# }
 
-variable "hackfest_provision_role_arn" {
-  description = "The ARN of the IAM role that has permissions to provision the resources for the hackfest."
-  type        = string
-}
+# variable "hackfest_provision_role_arn" {
+#   description = "The ARN of the IAM role that has permissions to provision the resources for the hackfest."
+#   type        = string
+# }
 
-variable "user_pool_id" {
-  description = "The ID of the Cognito User Pool"
-  type        = string
-}
+# variable "user_pool_id" {
+#   description = "The ID of the Cognito User Pool"
+#   type        = string
+# }
 
 # ------------------------------------------------------------------------------
 # OPTIONAL PARAMETERS
